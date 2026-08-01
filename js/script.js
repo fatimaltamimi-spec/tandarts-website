276d22588a1fafd8bdf97e47e09cd5b691363cac
fatimaltamimi-spec <fatimaltamimi@gmail.com>
2026-08-02 01:24:28 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,3 +16,9 @@ window.addEventListener("scroll", function () {
     lastScroll = currentScroll;
 
 });
+const menuButton = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".nav-menu");
+
+menuButton.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
