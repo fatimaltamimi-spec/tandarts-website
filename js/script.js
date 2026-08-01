dc2a8a900b6d9024d057f27bc3be55a815f9e67d
fatimaltamimi-spec <fatimaltamimi@gmail.com>
2026-08-02 01:18:10 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-// Mobiele header verbetering komt hier
+let lastScroll = 0;
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+
+    let currentScroll = window.pageYOffset;
+
+    if (currentScroll > lastScroll && currentScroll > 100) {
+        // naar beneden scrollen → header verbergen
+        header.classList.add("hide");
+    } else {
+        // omhoog scrollen → header terug
+        header.classList.remove("hide");
+    }
+
+    lastScroll = currentScroll;
+
+});
